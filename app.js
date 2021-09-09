ebcbd120f1901f3d775c2a8abfe42c1d4e4d487b
shreemaan-abhishek <shreemaan.abhishek@gmail.com>
2021-09-10 00:09:00 +0545

    Use particle.js callback instead of set timeout for canvas load event

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-particlesJS.load('particles-js', './particles.json', function() {
-    console.log('callback - particles.js config loaded');
-  });
 
-  var canvasElement = undefined
-  const topDiv = document.querySelector('.top')
+var canvasElement = undefined
+const topDiv = document.querySelector('.top')
 
-  setTimeout(() => {
-   canvasElement = document.querySelector('.particles-js-canvas-el')
-   console.log(topDiv.offsetHeight);
-   canvasElement.style.height = topDiv.offsetHeight+"px"
-  }, 55);
+particlesJS.load('particles-js', './particles.json', function () {
+    canvasElement = document.querySelector('.particles-js-canvas-el')
+    console.log(topDiv.offsetHeight);
+    canvasElement.style.height = topDiv.offsetHeight + "px"
+});
 
-  const setDims = () => {
-    canvasElement.style.height = topDiv.offsetHeight+"px"
-  }
 
-  window.addEventListener('resize', setDims)
+const setDims = () => {
+    canvasElement.style.height = topDiv.offsetHeight + "px"
+}
+
+window.addEventListener('resize', setDims)
